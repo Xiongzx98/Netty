ce11d91bcdc7dcc62b3340a2a94d46c3a05cba0b
Xiong <1783296281@qq.com>
2019-05-26 00:11:14 +0800

    Initial commit

---
+++ src/socket/IOClient.java
@@ -0,0 +1,33 @@
+package socket;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * @Author: Xiong
+ * @Date: 2019/5/25 10:55 PM
+ */
+public class IOClient {
+
+    Socket socket = null;
+
+    public IOClient(String IPAddress, int port) throws IOException {
+        socket = new Socket(IPAddress, port);
+    }
+
+    public static void main(String[] args) throws IOException {
+        IOClient client = new IOClient("127.0.0.1", 9999);
+    }
+
+    public void send(){
+        while (true){
+            try {
+                socket.getOutputStream().write((new Date() + ": hello world").getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
